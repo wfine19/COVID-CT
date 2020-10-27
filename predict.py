31e97a81057f74674f28f825fb7e0a19b874d7b3
William Fine <wfine@nd.edu>
2020-10-27 03:51:20 -0400

    Prediction files

---
+++ predict.py
@@ -0,0 +1,440 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import re
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as cp
+from collections import OrderedDict
+# from torchvision.models.utils import load_state_dict_from_url
+import errno
+import hashlib
+import os
+import re
+import shutil
+import sys
+import tempfile
+import torch
+import warnings
+import zipfile
+import torch
+import torchvision
+import torchvision.datasets as datasets
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+from torch.optim.lr_scheduler import StepLR
+import numpy as np
+from PIL import ImageFile
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+import pandas as pd
+import random 
+from shutil import copyfile
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import re
+import albumentations as albu
+from albumentations.pytorch import ToTensor
+from catalyst.data import Augmentor
+import torchxrayvision as xrv
+from torchvision import transforms, utils
+from sklearn.metrics import roc_auc_score
+from skimage.io import imread, imsave
+import skimage
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+train_transformer = transforms.Compose([
+    transforms.Resize(256),
+    transforms.RandomResizedCrop((224),scale=(0.5,1.0)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    normalize
+])
+
+val_transformer = transforms.Compose([
+    transforms.Resize(224),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    normalize
+])
+
+batchsize=10
+def read_txt(txt_path):
+    with open(txt_path) as f:
+        lines = f.readlines()
+    txt_data = [line.strip() for line in lines]
+    return txt_data
+
+class CovidCTDataset(Dataset):
+    def __init__(self, root_dir, txt_COVID, txt_NonCOVID, transform=None):
+        """
+        Args:
+            txt_path (string): Path to the txt file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        File structure:
+        - root_dir
+            - CT_COVID
+                - img1.png
+                - img2.png
+                - ......
+            - CT_NonCOVID
+                - img1.png
+                - img2.png
+                - ......
+        """
+        self.root_dir = root_dir
+        self.txt_path = [txt_COVID,txt_NonCOVID]
+        self.classes = ['CT_COVID', 'CT_NonCOVID']
+        self.num_cls = len(self.classes)
+        self.img_list = []
+        for c in range(self.num_cls):
+            cls_list = [[os.path.join(self.root_dir,self.classes[c],item), c] for item in read_txt(self.txt_path[c])]
+            self.img_list += cls_list
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_list)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_path = self.img_list[idx][0]
+        image = Image.open(img_path).convert('RGB')
+
+        if self.transform:
+            image = self.transform(image)
+        sample = {'img': image,
+                  'label': int(self.img_list[idx][1])}
+        return sample
+    
+if __name__ == '__main__':
+    trainset = CovidCTDataset(root_dir='new_data/4.4_image',
+                              txt_COVID='new_data/newtxt/train.txt',
+                              txt_NonCOVID='old_data/oldtxt/trainCT_NonCOVID.txt',
+                              transform= train_transformer)
+    valset = CovidCTDataset(root_dir='new_data/4.4_image',
+                              txt_COVID='new_data/newtxt/val.txt',
+                              txt_NonCOVID='old_data/oldtxt/valCT_NonCOVID.txt',
+                              transform= val_transformer)
+    testset = CovidCTDataset(root_dir='new_data/4.4_image',
+                              txt_COVID='new_data/newtxt/test.txt',
+                              txt_NonCOVID='old_data/oldtxt/testCT_NonCOVID.txt',
+                              transform= val_transformer)
+    print(trainset.__len__())
+    print(valset.__len__())
+    print(testset.__len__())
+
+    train_loader = DataLoader(trainset, batch_size=batchsize, drop_last=False, shuffle=True)
+    val_loader = DataLoader(valset, batch_size=batchsize, drop_last=False, shuffle=False)
+    test_loader = DataLoader(testset, batch_size=batchsize, drop_last=False, shuffle=False)
+
+
+alpha = None
+device = 'cpu'
+votenum = 10
+
+class _DenseLayer(nn.Sequential):
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate, memory_efficient=False):
+        super(_DenseLayer, self).__init__()
+        self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
+        self.add_module('relu1', nn.ReLU(inplace=True)),
+        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size *
+                                           growth_rate, kernel_size=1, stride=1,
+                                           bias=False)),
+        self.add_module('norm2', nn.BatchNorm2d(bn_size * growth_rate)),
+        self.add_module('relu2', nn.ReLU(inplace=True)),
+        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
+                                           kernel_size=3, stride=1, padding=1,
+                                           bias=False)),
+        self.drop_rate = drop_rate
+        self.memory_efficient = memory_efficient
+
+    def forward(self, *prev_features):
+        bn_function = _bn_function_factory(self.norm1, self.relu1, self.conv1)
+        if self.memory_efficient and any(prev_feature.requires_grad for prev_feature in prev_features):
+            bottleneck_output = cp.checkpoint(bn_function, *prev_features)
+        else:
+            bottleneck_output = bn_function(*prev_features)
+        new_features = self.conv2(self.relu2(self.norm2(bottleneck_output)))
+        if self.drop_rate > 0:
+            new_features = F.dropout(new_features, p=self.drop_rate,
+                                     training=self.training)
+        return new_features
+
+def _bn_function_factory(norm, relu, conv):
+    def bn_function(*inputs):
+        concated_features = torch.cat(inputs, 1)
+        bottleneck_output = conv(relu(norm(concated_features)))
+        return bottleneck_output
+
+    return bn_function
+
+class _DenseBlock(nn.Module):
+    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate, memory_efficient=False):
+        super(_DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(
+                num_input_features + i * growth_rate,
+                growth_rate=growth_rate,
+                bn_size=bn_size,
+                drop_rate=drop_rate,
+                memory_efficient=memory_efficient,
+            )
+            self.add_module('denselayer%d' % (i + 1), layer)
+
+    def forward(self, init_features):
+        features = [init_features]
+        for name, layer in self.named_children():
+            new_features = layer(*features)
+            features.append(new_features)
+        return torch.cat(features, 1)
+
+
+class _Transition(nn.Sequential):
+    def __init__(self, num_input_features, num_output_features):
+        super(_Transition, self).__init__()
+        self.add_module('norm', nn.BatchNorm2d(num_input_features))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
+                                          kernel_size=1, stride=1, bias=False))
+        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
+
+class DenseNet(nn.Module):
+    r"""Densenet-BC model class, based on
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
+
+    Args:
+        growth_rate (int) - how many filters to add each layer (`k` in paper)
+        block_config (list of 4 ints) - how many layers in each pooling block
+        num_init_features (int) - the number of filters to learn in the first convolution layer
+        bn_size (int) - multiplicative factor for number of bottle neck layers
+          (i.e. bn_size * k features in the bottleneck layer)
+        drop_rate (float) - dropout rate after each dense layer
+        num_classes (int) - number of classification classes
+        memory_efficient (bool) - If True, uses checkpointing. Much more memory efficient,
+          but slower. Default: *False*. See `"paper" <https://arxiv.org/pdf/1707.06990.pdf>`_
+    """
+
+    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
+                 num_init_features=64, bn_size=4, drop_rate=0, num_classes=4, memory_efficient=False):
+
+        super(DenseNet, self).__init__()
+
+        # First convolution
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
+                                padding=3, bias=False)),
+            ('norm0', nn.BatchNorm2d(num_init_features)),
+            ('relu0', nn.ReLU(inplace=True)),
+            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+        ]))
+
+        # Each denseblock
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(
+                num_layers=num_layers,
+                num_input_features=num_features,
+                bn_size=bn_size,
+                growth_rate=growth_rate,
+                drop_rate=drop_rate,
+                memory_efficient=memory_efficient
+            )
+            self.features.add_module('denseblock%d' % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                trans = _Transition(num_input_features=num_features,
+                                    num_output_features=num_features // 2)
+                self.features.add_module('transition%d' % (i + 1), trans)
+                num_features = num_features // 2
+
+        # Final batch norm
+        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+
+        # Linear layer
+        self.classifier = nn.Linear(num_features, num_classes)
+        self.num_features = num_features
+
+        # Official init from torch repo.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.constant_(m.bias, 0)
+    def change_cls_number(self,num_classes):
+        print("Changing Full Connected")
+        self.classifier = nn.Linear(self.num_features, num_classes)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1, 1))
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        return out
+
+def _densenet(arch, growth_rate, block_config, num_init_features, pretrained, progress,
+              **kwargs):
+
+    if pretrained:
+        number_cls = kwargs['num_classes']
+        kwargs['num_classes'] = 1000
+        model = DenseNet(growth_rate, block_config, num_init_features, **kwargs)
+        _load_state_dict(model, model_urls[arch], progress)
+        model.change_cls_number(number_cls)
+    else:
+        model = DenseNet(growth_rate, block_config, num_init_features, **kwargs)
+    return model
+
+def densenet169(pretrained=False, progress=True, **kwargs):
+    r"""Densenet-169 model from
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+        memory_efficient (bool) - If True, uses checkpointing. Much more memory efficient,
+          but slower. Default: *False*. See `"paper" <https://arxiv.org/pdf/1707.06990.pdf>`_
+    """
+     ## THIS IS WHERE WE EDIT THE NETWORK
+    return _densenet('densenet169', 32, (6, 12, 16, 16), 64, pretrained, progress,
+                     **kwargs)
+
+
+def test(epoch):
+    
+    model.eval()
+    test_loss = 0
+    correct = 0
+    results = []
+    
+    TP = 0
+    TN = 0
+    FN = 0
+    FP = 0
+    
+    
+    criteria = nn.CrossEntropyLoss()
+    # Don't update model
+    with torch.no_grad():
+        tpr_list = []
+        fpr_list = []
+        
+        predlist=[]
+        scorelist=[]
+        targetlist=[]
+        # Predict
+        for batch_index, batch_samples in enumerate(test_loader):
+            data, target = batch_samples['img'].to(device), batch_samples['label'].to(device)
+            output = model(data)
+            
+            test_loss += criteria(output, target.long())
+            score = F.softmax(output, dim=1)
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.long().view_as(pred)).sum().item()
+            targetcpu=target.long().cpu().numpy()
+            predlist=np.append(predlist, pred.cpu().numpy())
+            scorelist=np.append(scorelist, score.cpu().numpy()[:,1])
+            targetlist=np.append(targetlist,targetcpu)
+           
+    return targetlist, scorelist, predlist
+
+
+model = densenet169()
+pretrained_net = torch.load('model_backup/Dense169.pt')
+model.load_state_dict(pretrained_net)
+modelname = 'Dense169'
+print(model)
+
+
+# test
+bs = 10
+import warnings
+warnings.filterwarnings('ignore')
+
+r_list = []
+p_list = []
+acc_list = []
+AUC_list = []
+vote_pred = np.zeros(testset.__len__())
+vote_score = np.zeros(testset.__len__())
+
+#optimizer = optim.Adam(model.parameters(), lr=0.0001)
+#scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
+#scheduler = StepLR(optimizer, step_size=1)
+
+total_epoch = 1
+for epoch in range(1, total_epoch+1):
+    
+    targetlist, scorelist, predlist = test(epoch)
+
+    vote_pred = vote_pred + predlist 
+    vote_score = vote_score + scorelist 
+    
+    TP = ((predlist == 1) & (targetlist == 1)).sum()
+    TN = ((predlist == 0) & (targetlist == 0)).sum()
+    FN = ((predlist == 0) & (targetlist == 1)).sum()
+    FP = ((predlist == 1) & (targetlist == 0)).sum()
+
+    print('TP=',TP,'TN=',TN,'FN=',FN,'FP=',FP)
+    print('TP+FP',TP+FP)
+    p = TP / (TP + FP)
+    print('precision',p)
+    p = TP / (TP + FP)
+    r = TP / (TP + FN)
+    print('recall',r)
+    F1 = 2 * r * p / (r + p)
+    acc = (TP + TN) / (TP + TN + FP + FN)
+    print('F1',F1)
+    print('acc',acc)
+    AUC = roc_auc_score(targetlist, vote_score)
+    print('AUC', AUC)
+
+    if epoch % votenum == 0:
+        
+        # major vote
+        vote_pred[vote_pred <= (votenum/2)] = 0
+        vote_pred[vote_pred > (votenum/2)] = 1
+        
+        TP = ((vote_pred == 1) & (targetlist == 1)).sum()
+        TN = ((vote_pred == 0) & (targetlist == 0)).sum()
+        FN = ((vote_pred == 0) & (targetlist == 1)).sum()
+        FP = ((vote_pred == 1) & (targetlist == 0)).sum()
+        
+        print('TP=',TP,'TN=',TN,'FN=',FN,'FP=',FP)
+        print('TP+FP',TP+FP)
+        p = TP / (TP + FP)
+        print('precision',p)
+        p = TP / (TP + FP)
+        r = TP / (TP + FN)
+        print('recall',r)
+        F1 = 2 * r * p / (r + p)
+        acc = (TP + TN) / (TP + TN + FP + FN)
+        print('F1',F1)
+        print('acc',acc)
+        AUC = roc_auc_score(targetlist, vote_score)
+        print('AUC', AUC)
+        
+        vote_pred = np.zeros((1,testset.__len__()))
+        vote_score = np.zeros(testset.__len__())
+        print('vote_pred',vote_pred)
+        print('\n The epoch is {}, average recall: {:.4f}, average precision: {:.4f},average F1: {:.4f}, average accuracy: {:.4f}, average AUC: {:.4f}'.format(
+        epoch, r, p, F1, acc, AUC))
+
+        f = open(f'model_result/test_{modelname}.txt', 'a+')
+        f.write('\n The epoch is {}, average recall: {:.4f}, average precision: {:.4f},average F1: {:.4f}, average accuracy: {:.4f}, average AUC: {:.4f}'.format(
+        epoch, r, p, F1, acc, AUC))
+        f.close()
+
