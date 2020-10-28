84cded1a892361f2dbe79c89e0cb97fcf4014adf
William Fine <wfine@nd.edu>
2020-10-28 18:59:10 -0400

    added in visualization

---
+++ predict_and_viz.py
@@ -0,0 +1,384 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pylab as pl
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import tables
+import math
+import re
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as cp
+from collections import OrderedDict
+# from torchvision.models.utils import load_state_dict_from_url
+import errno
+import hashlib
+import os
+import re
+import shutil
+import sys
+import tempfile
+import torch
+import warnings
+import zipfile
+import torch
+import torchvision
+import torchvision.datasets as datasets
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+from torch.optim.lr_scheduler import StepLR
+import numpy as np
+from PIL import ImageFile
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+import pandas as pd
+import random 
+from shutil import copyfile
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import re
+import albumentations as albu
+from albumentations.pytorch import ToTensor
+from catalyst.data import Augmentor
+import torchxrayvision as xrv
+from torchvision import transforms, utils
+from sklearn.metrics import roc_auc_score
+from skimage.io import imread, imsave
+import skimage
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+train_transformer = transforms.Compose([
+    transforms.Resize(256),
+    transforms.RandomResizedCrop((224),scale=(0.5,1.0)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    normalize
+])
+
+val_transformer = transforms.Compose([
+    transforms.Resize(224),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    normalize
+])
+
+img_path = sys.argv[1]
+image = Image.open(img_path).convert('RGB')
+image = val_transformer(image)
+
+alpha = None
+device = 'cpu'
+votenum = 10
+
+class _DenseLayer(nn.Sequential):
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate, memory_efficient=False):
+        super(_DenseLayer, self).__init__()
+        self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
+        self.add_module('relu1', nn.ReLU(inplace=True)),
+        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size *
+                                           growth_rate, kernel_size=1, stride=1,
+                                           bias=False)),
+        self.add_module('norm2', nn.BatchNorm2d(bn_size * growth_rate)),
+        self.add_module('relu2', nn.ReLU(inplace=True)),
+        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
+                                           kernel_size=3, stride=1, padding=1,
+                                           bias=False)),
+        self.drop_rate = drop_rate
+        self.memory_efficient = memory_efficient
+
+    def forward(self, *prev_features):
+        bn_function = _bn_function_factory(self.norm1, self.relu1, self.conv1)
+        if self.memory_efficient and any(prev_feature.requires_grad for prev_feature in prev_features):
+            bottleneck_output = cp.checkpoint(bn_function, *prev_features)
+        else:
+            bottleneck_output = bn_function(*prev_features)
+        new_features = self.conv2(self.relu2(self.norm2(bottleneck_output)))
+        if self.drop_rate > 0:
+            new_features = F.dropout(new_features, p=self.drop_rate,
+                                     training=self.training)
+        return new_features
+
+def _bn_function_factory(norm, relu, conv):
+    def bn_function(*inputs):
+        concated_features = torch.cat(inputs, 1)
+        bottleneck_output = conv(relu(norm(concated_features)))
+        return bottleneck_output
+
+    return bn_function
+
+class _DenseBlock(nn.Module):
+    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate, memory_efficient=False):
+        super(_DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(
+                num_input_features + i * growth_rate,
+                growth_rate=growth_rate,
+                bn_size=bn_size,
+                drop_rate=drop_rate,
+                memory_efficient=memory_efficient,
+            )
+            self.add_module('denselayer%d' % (i + 1), layer)
+
+    def forward(self, init_features):
+        features = [init_features]
+        for name, layer in self.named_children():
+            new_features = layer(*features)
+            features.append(new_features)
+        return torch.cat(features, 1)
+
+
+class _Transition(nn.Sequential):
+    def __init__(self, num_input_features, num_output_features):
+        super(_Transition, self).__init__()
+        self.add_module('norm', nn.BatchNorm2d(num_input_features))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
+                                          kernel_size=1, stride=1, bias=False))
+        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
+
+class DenseNet(nn.Module):
+    r"""Densenet-BC model class, based on
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
+
+    Args:
+        growth_rate (int) - how many filters to add each layer (`k` in paper)
+        block_config (list of 4 ints) - how many layers in each pooling block
+        num_init_features (int) - the number of filters to learn in the first convolution layer
+        bn_size (int) - multiplicative factor for number of bottle neck layers
+          (i.e. bn_size * k features in the bottleneck layer)
+        drop_rate (float) - dropout rate after each dense layer
+        num_classes (int) - number of classification classes
+        memory_efficient (bool) - If True, uses checkpointing. Much more memory efficient,
+          but slower. Default: *False*. See `"paper" <https://arxiv.org/pdf/1707.06990.pdf>`_
+    """
+
+    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
+                 num_init_features=64, bn_size=4, drop_rate=0, num_classes=4, memory_efficient=False):
+
+        super(DenseNet, self).__init__()
+
+        # First convolution
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
+                                padding=3, bias=False)),
+            ('norm0', nn.BatchNorm2d(num_init_features)),
+            ('relu0', nn.ReLU(inplace=True)),
+            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+        ]))
+
+        # Each denseblock
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(
+                num_layers=num_layers,
+                num_input_features=num_features,
+                bn_size=bn_size,
+                growth_rate=growth_rate,
+                drop_rate=drop_rate,
+                memory_efficient=memory_efficient
+            )
+            self.features.add_module('denseblock%d' % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                trans = _Transition(num_input_features=num_features,
+                                    num_output_features=num_features // 2)
+                self.features.add_module('transition%d' % (i + 1), trans)
+                num_features = num_features // 2
+
+        # Final batch norm
+        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+
+        # Linear layer
+        self.classifier = nn.Linear(num_features, num_classes)
+        self.num_features = num_features
+
+        # Official init from torch repo.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.constant_(m.bias, 0)
+    def change_cls_number(self,num_classes):
+        print("Changing Full Connected")
+        self.classifier = nn.Linear(self.num_features, num_classes)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1, 1))
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        return out
+
+def _densenet(arch, growth_rate, block_config, num_init_features, pretrained, progress,
+              **kwargs):
+
+    if pretrained:
+        number_cls = kwargs['num_classes']
+        kwargs['num_classes'] = 1000
+        model = DenseNet(growth_rate, block_config, num_init_features, **kwargs)
+        _load_state_dict(model, model_urls[arch], progress)
+        model.change_cls_number(number_cls)
+    else:
+        model = DenseNet(growth_rate, block_config, num_init_features, **kwargs)
+    return model
+
+def densenet169(pretrained=False, progress=True, **kwargs):
+    r"""Densenet-169 model from
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+        memory_efficient (bool) - If True, uses checkpointing. Much more memory efficient,
+          but slower. Default: *False*. See `"paper" <https://arxiv.org/pdf/1707.06990.pdf>`_
+    """
+     ## THIS IS WHERE WE EDIT THE NETWORK
+    return _densenet('densenet169', 32, (6, 12, 16, 16), 64, pretrained, progress,
+                     **kwargs)
+
+
+model = densenet169()
+pretrained_net = torch.load('model_backup/Dense169.pt')
+model.load_state_dict(pretrained_net)
+modelname = 'Dense169'
+#print(model)
+
+
+# test
+bs = 10
+import warnings
+warnings.filterwarnings('ignore')
+
+def test():
+    
+    model.eval()
+    
+    
+    criteria = nn.CrossEntropyLoss()
+    # Don't update model
+    with torch.no_grad():
+        tpr_list = []
+        fpr_list = []
+        
+        # Predict
+        data = torch.unsqueeze(image, 0)
+        output = model(data)
+        
+        score = F.softmax(output, dim=1)
+        pred = output.argmax(dim=1, keepdim=True)
+        if pred[0] == 1:
+          print("Positive")
+          return "Positive"
+        else:
+          print("Negative")
+          return "Negative"
+
+
+
+
+label = test()
+
+class CamExtractor():
+    """
+        Extracts cam features from the model
+    """
+    def __init__(self, model, target_layer):
+        self.model = model
+        self.target_layer = target_layer
+        self.gradients = None
+
+    def save_gradient(self, grad):
+        self.gradients = grad
+
+    def forward_pass_on_convolutions(self, x):
+        """
+            Does a forward pass on convolutions, hooks the function at given layer
+        """
+        conv_output = None
+        for module_pos, module in enumerate(self.model.features._modules.items()):
+            x = module[1](x)  # Forward
+            if int(module_pos) == self.target_layer:
+                x.register_hook(self.save_gradient)
+                conv_output = x  # Save the convolution output on that layer
+        return conv_output, x
+
+    def forward_pass(self, x):
+        """
+            Does a full forward pass on the model
+        """
+        # Forward pass on the convolutions
+        conv_output, x = self.forward_pass_on_convolutions(x)
+        #x = x.view(x.size(0), -1)  # Flatten
+        x = F.relu(x, inplace=True)
+        x = F.adaptive_avg_pool2d(x, (1, 1)).view(x.size(0), -1)
+        # Forward pass on the classifier
+        x = self.model.classifier(x)
+        return conv_output, x
+
+class GradCam():
+    """
+        Produces class activation map
+    """
+    def __init__(self, model, device, target_layer):
+        self.device = device
+        self.model = model.to(device)
+        self.model.eval()
+        # Define extractor
+        self.extractor = CamExtractor(self.model, target_layer)
+
+    def generate_cam(self, input_image, target_class=None):
+        # Full forward pass
+        # conv_output is the output of convolutions at specified layer
+        # model_output is the final output of the model (1, 1000)
+        conv_output, model_output = self.extractor.forward_pass(input_image)
+        if target_class is None:
+            target_class = np.argmax(model_output.data.numpy())
+        # Target for backprop
+        one_hot_output = torch.FloatTensor(1, model_output.size()[-1]).zero_().to(self.device)
+        one_hot_output[0] = 1
+        # Zero grads
+        self.model.features.zero_grad()
+        self.model.classifier.zero_grad()
+        # Backward pass with specified target
+        model_output.backward(gradient=one_hot_output, retain_graph=True)
+        # Get hooked gradients
+        guided_gradients = self.extractor.gradients.data.cpu().numpy()[0]
+        # Get convolution outputs
+        target = conv_output.data.cpu().numpy()[0]
+        # Get weights from gradients
+        weights = np.mean(guided_gradients, axis=(1, 2))  # Take averages for each gradient
+        # Create empty numpy array for cam
+        cam = np.ones(target.shape[1:], dtype=np.float32)
+        # Multiply each weight with its conv output and then, sum
+        for i, w in enumerate(weights):
+            cam += w * target[i, :, :]
+        cam = cv2.resize(cam, (224, 224))
+        cam = np.maximum(cam, 0)
+        cam = (cam - np.min(cam)) / (np.max(cam) - np.min(cam))  # Normalize between 0-1
+        cam = np.uint8(cam * 255)  # Scale between 0-255 to visualize
+        return cam
+
+nlayers=len(model.features._modules.items())-1
+nlayers=1
+fig, ax = plt.subplots(math.ceil(nlayers/4),1, figsize=(20,10))
+#ax = ax.flatten()
+
+
+grad_cam = GradCam(model, device,target_layer=1)
+cam = grad_cam.generate_cam(image[None,::], label)
+ax.imshow(cam)
+pl.show()
+image=Image.open(img_path)
+image.show()
